f017e5f65bc57708e1de469d85f9847a52c86da8
harrison <harrison.vanderbyl@gmail.com>
2023-11-10 14:45:40 -0800

    add quantizer

--- quant.py
+++ quant.py
@@ -1,4 +1,28 @@
 # for use if the converter crashes before quantization
 import onnx
 from onnxruntime.quantization import quantize_dynamic, QuantType
-quantize_dynamic("RWKV_24_2048_32_15.onnx","RWKV_24_2048_32_15_quant.onnx", per_channel=True, reduce_range=True, use_external_data_format=False, weight_type=QuantType.QUInt8)
+import os
+files = [f for f in os.listdir('.') if os.path.isfile(f)]
+files = [f for f in files if f.endswith(".onnx") or f.endswith(".ort")]
+import inquirer
+model = inquirer.list_input("Select model", choices=files)
+
+choices = inquirer.checkbox(
+        "Select quantization options (use space bar to select checkboxes)", choices=["per_channel(slow, more acurate)", "reduce_range(sometimes more accurate)", "use_external_data_format(use if model larger than 2B)"], default=["use_external_data_format(use if model larger than 2B)"])
+
+reduce_range = False
+per_channel = False
+use_external_data_format = False
+if "per_channel(slower, more acurate)" in choices:
+    per_channel = True
+if "reduce_range(sometimes more accurate)" in choices:
+    reduce_range = True
+if "use_external_data_format(use if model larger than 2B)" in choices:
+    use_external_data_format = True
+
+# choose quantization type (default is QUInt8) // choose 1
+qtype = inquirer.list_input("Select quantization type", choices=[e for e in QuantType], default=QuantType.QUInt8.value )
+qtype = QuantType(qtype)
+endoption = f'_{qtype}-{"pc" if per_channel else "pc"}-{"rr" if reduce_range else "norr"}-{"ext" if use_external_data_format else "noext"}.onnx'
+
+quantize_dynamic(model,model.replace(".onnx",endoption), per_channel=per_channel, reduce_range=reduce_range, use_external_data_format=use_external_data_format, weight_type=qtype)
